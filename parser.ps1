3fd8a52891d8b31dd9bd02c385088889c8440030
JoramM <jorammarkert@gmx.de>
2017-12-22 13:56:07 +0100

    Handle forgotten names

--- parser.ps1
+++ parser.ps1
@@ -2,6 +2,11 @@ param(
   [string]$infile
 )
 
+function rm_to_space($in){
+  $pos = $in.IndexOf(' ')
+  return $in.SubString($pos)
+}
+
 Write-Host
 Write-Host "========================"
 
@@ -29,39 +34,54 @@ Write-Host
 
 $line_nr = 1
 while($null -ne ($line = $reader.ReadLine())) {
+    #echo $line_nr
     if($line.length -ne 0){
+
+      # #<Time># <Name> <Symbol[<,>,#] optional> <Content>
       if($line.SubString(0,1) -eq "#"){
-        # #<Time># <Name> <Symbol[<,>,#] optional> <Content>
         $time = $line.SubString(2,4)
-        $name = $line.Split(':',3)[1]
-        $name = $name.substring($name.length - 1,1)
-        # symbol is extracted later
-        $content = $line.Split(':',3)[2]
+        #[regex]$regex = ':'
+        #$regex.matches($a).count
+        $name = rm_to_space($line.Split(':',3)[1])
+        if ($line.Split(':',3).length -gt 2){
+          $content = $line.Split(':',3)[2]
+        } else {
+          $content = $name
+          $name = "not found"
+        }
       }
+
+      # <Name>: #<Time># <Symbol[<,>,#] optional> <Content>
       if($line.SubString(0,1) -ne "#"){
-        # <Name>: #<Time># <Symbol[<,>,#] optional> <Content>
-        $name = $line.Split(':',2)[0]
+        $name = rm_to_space($line.Split(':',2)[0])
+        if ($line.Split(':',3).length -gt 2){
+          $content = $line.Split('#',3)[2]
+        } else {
+          $content = $name
+          $name = "not found"
+        }
         $time = $line.Split('#',3)[1]
-        $content = $line.Split('#',3)[2]
       }
 
       # Some formatting stuff
       # remove whitespaces at the beginning
-      while($content.StartsWith(" ")){
-        $content = $content.TrimStart(" ")
-      }
+      if ($content -ne $null){
+        while($content.StartsWith(" ")){
+          $content = $content.TrimStart(" ")
+        }
 
-      # check if there is a symbol at the beginning of the content
-      if($content.StartsWith('#') -or $content.StartsWith('>') -or $content.StartsWith('<')){
-        $symbol = $content.SubString(0,1)
-        $content = $content.TrimStart($symbol + " ")
-      }else{
-        $symbol = ""
-      }
+        # check if there is a symbol at the beginning of the content
+        if($content.StartsWith('#') -or $content.StartsWith('>') -or $content.StartsWith('<')){
+          $symbol = $content.SubString(0,1)
+          $content = $content.TrimStart($symbol + " ")
+        }else{
+          $symbol = ""
+        }
 
-      # remove whitespaces at the beginning
-      while($content.StartsWith(" ")){
-        $content = $content.TrimStart(" ")
+        # remove whitespaces at the beginning
+        while($content.StartsWith(" ")){
+          $content = $content.TrimStart(" ")
+        }
       }
 
       # save row data into an object
