0a7b1d5c303cab21b2cd5ab0c3375d93e3de711a
JoramM <jorammarkert@gmx.de>
2017-12-22 12:34:59 +0100

    Add support for different input formats

--- README.md
+++ README.md
@@ -8,9 +8,22 @@ Run the PowerShell and set the Execuation-Policy to Unrestricted
 Set-ExecutionPolicy Unrestricted
 ```
 
+## Supported Input Format
+```
+#<Time># <Name>: <Symbol[<,>,#] optional> <Content>
+```
+also supported:
+```
+<Name>: #<Time># <Symbol[<,>,#] optional> <Content>
+```
+
 ## Run the parser
-1. `cd` into the directory with the `parser.ps1` file
-2. Run the script. Do not forget to set the `-infile` parameter, which expects the path to your (`*.txt`) transcript file
+1. Download the repository or at least the `parser.ps1` file
+2. `cd` into the directory with the `parser.ps1` file e.g.:
+```
+cd C:\Users\User\dev\transcript-parser
+```
+3. Run the script. Do not forget to set the `-infile` parameter, which expects the path to your (`*.txt`) transcript file
 ```PowerShell
 parser.ps1 -infile 'path/to/transcript.txt'
 ```
--- parser.ps1
+++ parser.ps1
@@ -15,7 +15,7 @@ if ($infile -eq ""){
 }
 Write-Host "Eingabedatei:" $infile
 
-$CsvEncoding = "UTF8"
+$csv_encoding = "UTF8"
 
 $curr_dir = (Get-Item -Path ".\" -Verbose).FullName
 $reader = [System.IO.File]::OpenText($curr_dir + "\" + $infile)
@@ -29,23 +29,39 @@ Write-Host
 
 $line_nr = 1
 while($null -ne ($line = $reader.ReadLine())) {
-    $name = $line.Split(':',2)[0]
+    if($line.length -ne 0){
+      if($line.SubString(0,1) -eq "#"){
+        # #<Time># <Name> <Symbol[<,>,#] optional> <Content>
+        $time = $line.SubString(2,4)
+        $name = $line.Split(':',3)[1]
+        $name = $name.substring($name.length - 1,1)
+        # symbol is extracted later
+        $content = $line.Split(':',3)[2]
+      }
+      if($line.SubString(0,1) -ne "#"){
+        # <Name>: #<Time># <Symbol[<,>,#] optional> <Content>
+        $name = $line.Split(':',2)[0]
+        $time = $line.Split('#',3)[1]
+        $content = $line.Split('#',3)[2]
+      }
 
-    if($name -ne ""){
-      $parts= $line.Split('#',3)
-      $time = $parts[1]
-
-      $content = $parts[2]
+      # Some formatting stuff
       # remove whitespaces at the beginning
       while($content.StartsWith(" ")){
         $content = $content.TrimStart(" ")
       }
+
       # check if there is a symbol at the beginning of the content
       if($content.StartsWith('#') -or $content.StartsWith('>') -or $content.StartsWith('<')){
         $symbol = $content.SubString(0,1)
         $content = $content.TrimStart($symbol + " ")
       }else{
         $symbol = ""
+      }
+
+      # remove whitespaces at the beginning
+      while($content.StartsWith(" ")){
+        $content = $content.TrimStart(" ")
       }
 
       # save row data into an object
@@ -56,15 +72,14 @@ while($null -ne ($line = $reader.ReadLine())) {
         Symbol = $symbol
         Inhalt = $content
       }
-
       $result += $newrow
       $line_nr++
     }
 }
 
 # write result to a csv file
-$result | Select-Object Nr, Zeit, Name, Symbol, Inhalt | Export-Csv $outfile -notypeinformation -Delimiter ";" -Encoding $CsvEncoding
+$result | Select-Object Nr, Zeit, Name, Symbol, Inhalt | Export-Csv $outfile -notypeinformation -Delimiter ";" -Encoding $csv_encoding
 
-Write-Host "ferdsch!"
+Write-Host "ferdsch! (" ($line_nr-1) "Zeilen )"
 Write-Host "========================"
 Write-Host
